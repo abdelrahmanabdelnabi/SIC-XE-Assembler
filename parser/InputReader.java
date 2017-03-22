2b4758f2a795336b0b8faec6253ad15f15f23378
Abdelrahman <abdoumohamed96@gmail.com>
2017-03-22 18:21:52 +0200

    add docs for InputReader

--- parser/InputReader.java
+++ parser/InputReader.java
@@ -5,9 +5,21 @@ import java.io.*;
 /**
  * Created by abdelrahman on 3/22/17.
  */
+
+/**
+ * Reads the input assembly instructions from an input stream
+ * the input stream is either a file or a string
+ */
 public class InputReader {
     BufferedReader reader;
 
+    /**
+     * Creates a buffered input reader that either reads from a file or from a string
+     *
+     * @param type the source of the input, either file or string
+     * @param input the file name in case the input type is file, or
+     *              the input itself in case the input type is string.
+     */
     public InputReader(InputType type, String input) {
         if (type == InputType.File) {
             try {
@@ -20,6 +32,9 @@ public class InputReader {
         }
     }
 
+    /**
+     * returns the next line in the input
+     */
     public String getLine() throws IOException {
         return reader.readLine();
     }
