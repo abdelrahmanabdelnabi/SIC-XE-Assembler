f941b3161f898272b2c0e187510e796a98c477d2
Abdelrahman <abdoumohamed96@gmail.com>
2017-03-22 04:18:04 +0200

    initial commit: create packages

---
+++ assembler/Instruction.java
@@ -0,0 +1,10 @@
+package assembler;
+
+/**
+ * Created by abdelrahman on 3/22/17.
+ */
+public class Instruction {
+    private String label;
+    private String opCode;
+    private String operand;
+}
