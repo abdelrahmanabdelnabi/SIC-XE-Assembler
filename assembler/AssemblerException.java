ffcbe953e1b437059fca9f8d349324df0f85f804
Abdelrahman <abdoumohamed96@gmail.com>
2017-04-07 22:47:30 +0200

    add Assembler Excpetion class, used to throw exceptions when encoutering errors during assembly

---
+++ assembler/AssemblerException.java
@@ -0,0 +1,11 @@
+package assembler;
+
+/**
+ * Created by abdelrahman on 4/7/17.
+ */
+public class AssemblerException extends RuntimeException {
+    
+    public AssemblerException(String message) {
+        super(message);
+    }
+}
