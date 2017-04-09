ce8483bcc2064d044c9856f66b9fb81aefb23556
Abdelrahman <abdoumohamed96@gmail.com>
2017-04-09 20:32:47 +0200

    update class Format3_4Builder

--- assembler/utils/Format3_4Builder.java
+++ assembler/utils/Format3_4Builder.java
@@ -1,8 +1,45 @@
 package assembler.utils;
 
+import assembler.datastructures.Format;
+
 /**
  * Created by abdelrahman on 4/9/17.
  */
-public class Format3_4Builder {
-    
+public class Format3_4Builder extends AbstractInstructionBuilder{
+    private int operand;
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    @Override
+    public void setIndirect(boolean isIndirect) {
+
+    }
+
+    @Override
+    public void setImmediate(boolean isImmediate) {
+
+    }
+
+    @Override
+    public void setIndexed(boolean isIndirect) {
+
+    }
+
+    @Override
+    public void setBaseRelative(boolean isBaseRelative) {
+
+    }
+
+    @Override
+    public void setPCRelative(boolean isPCRelative) {
+
+    }
+
+    @Override
+    public Format getFormat() {
+        return null;
+    }
 }
