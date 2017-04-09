7182640074c083b7c84151e8de113379d7cf9e63
Abdelrahman <abdoumohamed96@gmail.com>
2017-04-09 21:23:23 +0200

    add Parser Tests

---
+++ parser/ParserTest.java
@@ -0,0 +1,61 @@
+package parser;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by abdelrahman on 4/9/17.
+ */
+public class ParserTest {
+
+    private static final String OPCODE               =         "TIXR"          ;
+    private static final String OPCODE_LABEL         = "LOOP    STA"           ;
+    private static final String OPCODE_OPERAND       =         "STA     BUFFER";
+    private static final String OPCODE_LABEL_OPERAND = "LOOP    STA    BUFFER" ;
+
+
+    @org.junit.Test
+    public void testOpcodeOnly() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testOpcodeAndOperand() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testOpcodeOperandAndLabel() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testOpcodeAndLabel() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testDirectiveOnly() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testDirectiveAndLabel() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testDirectiveAndOperand() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testDirectiveLabelAndOperand() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testCommendLine() throws Exception {
+
+    }
+
+}
