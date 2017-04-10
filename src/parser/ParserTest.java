3e1fe1101f4695f941b2828539bad3ab04c5a188
Abdelrahman <abdoumohamed96@gmail.com>
2017-04-10 21:34:14 +0200

    use a relative path for the test file to work on any machine

--- src/parser/ParserTest.java
+++ src/parser/ParserTest.java
@@ -12,23 +12,20 @@ import static src.parser.InputReader.InputType;
  * Created by abdelrahman on 4/9/17.
  */
 public class ParserTest {
-    private String path = "/home/ahmed/Desktop/SIC-XE-Assembler/src.tests/parsingTest.asm";
+    private String relativePath = System.getProperty("user.dir");
+    private String testFilePath = relativePath + "/src/tests/parsingTest.asm";
     private Parser parser;
     private InputReader reader;
 
     @Before
     public void setUp() {
-        reader = new InputReader(InputType.File, path);
+        reader = new InputReader(InputType.File, testFilePath);
         parser = new Parser(reader);
-        OpcodeTable.getOpcodeTable();
     }
 
     @Test
     public void testReader_Parser() {
         parser.parse();
-//        for (Instruction inst : src.parser.getParsedInstuctions()) {
-//            System.out.println(inst);
-//        }
         System.out.println(Arrays.toString(parser.getParsedInstuctions().toArray()));
     }
 
