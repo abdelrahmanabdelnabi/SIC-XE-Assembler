3cd6058635dbcee9095d68b249e0463e2ac36585
Abdelrahman <abdoumohamed96@gmail.com>
2017-04-10 23:56:47 +0200

    add some error strings

--- src/assembler/ErrorStrings.java
+++ src/assembler/ErrorStrings.java
@@ -4,6 +4,8 @@ package src.assembler;
  * Created by abdelrahman on 4/7/17.
  */
 public class ErrorStrings {
-    public static String INVALID_NUMBER_FORMAT = "not a legal number";
-    public static String LABEL_REDEFINITION    = "Label already defined before, label cannot be defined twice";
+    public static String INVALID_NUMBER_FORMAT = "Not a legal number";
+    public static String LABEL_REDEFINITION = "Label already defined before, label cannot be defined twice";
+    public static String UNDEFINED_MNEMONIC = "Undefined nmemonic";
+    public static String INVALID_PLUS_SIGN_USE = "Invalid use of plus prefix (+). The (+) prefix can not be used with Format 1 or 2 instructions";
 }
