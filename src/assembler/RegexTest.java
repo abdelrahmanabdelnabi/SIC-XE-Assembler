00e553ef9f1637b5a6a196113237efc25ca65a8e
Abdelrahman <abdoumohamed96@gmail.com>
2017-04-21 22:34:00 +0200

    add some testing for regular expressions

--- src/assembler/RegexTest.java
+++ src/assembler/RegexTest.java
@@ -1,7 +1,48 @@
 package src.assembler;
 
+import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by abdelrahman on 4/21/17.
  */
 public class RegexTest {
+
+    static String onlyLetters = "labEL";
+
+    static String inDirect = "@LABEL";
+
+    static String immediate = "#Label";
+
+    @Test
+    public void testOperandIsLabel() {
+
+        assertEquals(onlyLetters.matches("[a-zA-Z]+"), true);
+        assertEquals(inDirect.matches("[a-zA-Z]+"), false);
+        assertEquals(immediate.matches("[a-zA-Z]+"), false);
+
+    }
+
+    @Test
+    public void testOperandIsInDirect() {
+
+        assertEquals(inDirect.matches("@[a-zA-Z]+"), true);
+        assertEquals(onlyLetters.matches("@[a-zA-Z]+"), false);
+        assertEquals(immediate.matches("@[a-zA-Z]+"), false);
+
+    }
+
+    @Test
+    public void testOperandIsImmediate() {
+        boolean matches = immediate.matches("#[a-zA-Z]+");
+
+        assertEquals(immediate.matches("#[a-zA-Z]+"), true);
+        assertEquals(onlyLetters.matches("#[a-zA-Z]+"), false);
+        assertEquals(inDirect.matches("#[a-zA-Z]+"), false);
+    }
+
 }
