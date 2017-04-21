65bf18aeab17ef65e93085d06e21c676ee5ba434
Ahmed Lotfey <a7med.lotfey@gmail.com>
2017-04-21 15:10:12 +0200

    fix opcode table

--- src/assembler/datastructures/OpcodeTable.java
+++ src/assembler/datastructures/OpcodeTable.java
@@ -2,6 +2,8 @@ package src.assembler.datastructures;
 
 import java.util.*;
 
+import static src.assembler.datastructures.OperandType.*;
+
 /**
  * Created by abdelrahman on 4/7/17.
  */
@@ -51,66 +53,77 @@ public class OpcodeTable {
     }
 
     private static void fillOpcodeTable() {
-        // TODO: insert instruction names and their properties
-        opCodeTable.put("ADD", new InstProp(24, Format.FORMAT3));
-        opCodeTable.put("ADDF", new InstProp(88, Format.FORMAT3));
-        opCodeTable.put("ADDR", new InstProp(144, Format.FORMAT2));
-        opCodeTable.put("AND", new InstProp(64, Format.FORMAT3));
-        opCodeTable.put("CLEAR", new InstProp(180, Format.FORMAT2));
-        opCodeTable.put("COMP", new InstProp(40, Format.FORMAT3));
-        opCodeTable.put("COMPF", new InstProp(136, Format.FORMAT3));
-        opCodeTable.put("COMPR", new InstProp(160, Format.FORMAT2));
-        opCodeTable.put("DIV", new InstProp(36, Format.FORMAT3));
-        opCodeTable.put("DIVF", new InstProp(100, Format.FORMAT3));
-        opCodeTable.put("DIVR", new InstProp(156, Format.FORMAT2));
-        opCodeTable.put("FIX", new InstProp(196, Format.FORMAT1));
-        opCodeTable.put("FLOAT", new InstProp(192, Format.FORMAT1));
-        opCodeTable.put("HIO", new InstProp(244, Format.FORMAT1));
-        opCodeTable.put("J", new InstProp(60, Format.FORMAT3));
-        opCodeTable.put("JEQ", new InstProp(48, Format.FORMAT3));
-        opCodeTable.put("JGT", new InstProp(52, Format.FORMAT3));
-        opCodeTable.put("JLT", new InstProp(56, Format.FORMAT3));
-        opCodeTable.put("JSUB", new InstProp(72, Format.FORMAT3));
-        opCodeTable.put("LDA", new InstProp(0, Format.FORMAT3));
-        opCodeTable.put("LDB", new InstProp(104, Format.FORMAT3));
-        opCodeTable.put("LDCH", new InstProp(80, Format.FORMAT3));
-        opCodeTable.put("LDF", new InstProp(112, Format.FORMAT3));
-        opCodeTable.put("LDL", new InstProp(8, Format.FORMAT3));
-        opCodeTable.put("LDS", new InstProp(108, Format.FORMAT3));
-        opCodeTable.put("LDT", new InstProp(116, Format.FORMAT3));
-        opCodeTable.put("LDX", new InstProp(4, Format.FORMAT3));
-        opCodeTable.put("LPS", new InstProp(208, Format.FORMAT3));
-        opCodeTable.put("MUL", new InstProp(32, Format.FORMAT3));
-        opCodeTable.put("MULF", new InstProp(96, Format.FORMAT3));
-        opCodeTable.put("MULR", new InstProp(152, Format.FORMAT2));
-        opCodeTable.put("NORM", new InstProp(200, Format.FORMAT1));
-        opCodeTable.put("OR", new InstProp(68, Format.FORMAT3));
-        opCodeTable.put("RD", new InstProp(216, Format.FORMAT3));
-        opCodeTable.put("RMO", new InstProp(172, Format.FORMAT2));
-        opCodeTable.put("RSUB", new InstProp(76, Format.FORMAT3));
-        opCodeTable.put("SHIFTL", new InstProp(164, Format.FORMAT2));
-        opCodeTable.put("SHIFTR", new InstProp(168, Format.FORMAT2));
-        opCodeTable.put("SIO", new InstProp(240, Format.FORMAT1));
-        opCodeTable.put("SUBR", new InstProp(148, Format.FORMAT2));
-        opCodeTable.put("SVC", new InstProp(176, Format.FORMAT2));
-        opCodeTable.put("TD", new InstProp(224, Format.FORMAT3));
-        opCodeTable.put("TIO", new InstProp(248, Format.FORMAT1));
-        opCodeTable.put("TIX", new InstProp(44, Format.FORMAT3));
-        opCodeTable.put("TIXR", new InstProp(184, Format.FORMAT2));
-        opCodeTable.put("WD", new InstProp(220, Format.FORMAT3));
-        opCodeTable.put("SSK", new InstProp(236, Format.FORMAT3));
-        opCodeTable.put("STA", new InstProp(12, Format.FORMAT3));
-        opCodeTable.put("STB", new InstProp(120, Format.FORMAT3));
-        opCodeTable.put("STCH", new InstProp(84, Format.FORMAT3));
-        opCodeTable.put("STF", new InstProp(128, Format.FORMAT3));
-        opCodeTable.put("STI", new InstProp(212, Format.FORMAT3));
-        opCodeTable.put("STL", new InstProp(20, Format.FORMAT3));
-        opCodeTable.put("STS", new InstProp(124, Format.FORMAT3));
-        opCodeTable.put("STSW", new InstProp(232, Format.FORMAT3));
-        opCodeTable.put("STT", new InstProp(132, Format.FORMAT3));
-        opCodeTable.put("STX", new InstProp(16, Format.FORMAT3));
-        opCodeTable.put("SUB", new InstProp(28, Format.FORMAT3));
-        opCodeTable.put("SUBF", new InstProp(92, Format.FORMAT3));
+        // TODO: to be reviewed
+
+        //FORMAT 1
+        opCodeTable.put("FIX", new InstProp(196, Format.FORMAT1, NONE, NONE));
+        opCodeTable.put("FLOAT", new InstProp(192, Format.FORMAT1, NONE, NONE));
+        opCodeTable.put("HIO", new InstProp(244, Format.FORMAT1, NONE, NONE));
+        opCodeTable.put("NORM", new InstProp(200, Format.FORMAT1, NONE, NONE));
+        opCodeTable.put("SIO", new InstProp(240, Format.FORMAT1, NONE, NONE));
+        opCodeTable.put("TIO", new InstProp(248, Format.FORMAT1, NONE, NONE));
+
+
+        //FORMAT 2
+        opCodeTable.put("CLEAR", new InstProp(180, Format.FORMAT2, REGISTER, NONE));
+        opCodeTable.put("TIXR", new InstProp(184, Format.FORMAT2, REGISTER, NONE));
+
+        opCodeTable.put("SVC", new InstProp(176, Format.FORMAT2, VALUE, NONE));
+
+        opCodeTable.put("SHIFTL", new InstProp(164, Format.FORMAT2, REGISTER, VALUE));
+        opCodeTable.put("SHIFTR", new InstProp(168, Format.FORMAT2, REGISTER, VALUE));
+
+        opCodeTable.put("ADDR", new InstProp(144, Format.FORMAT2, REGISTER, REGISTER));
+        opCodeTable.put("COMPR", new InstProp(160, Format.FORMAT2, REGISTER, REGISTER));
+        opCodeTable.put("DIVR", new InstProp(156, Format.FORMAT2, REGISTER, REGISTER));
+        opCodeTable.put("MULR", new InstProp(152, Format.FORMAT2, REGISTER, REGISTER));
+        opCodeTable.put("SUBR", new InstProp(148, Format.FORMAT2, REGISTER, REGISTER));
+        opCodeTable.put("RMO", new InstProp(172, Format.FORMAT2, REGISTER, REGISTER));
+
+
+        // FORMAT 3 & 4
+        opCodeTable.put("ADD", new InstProp(24, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("ADDF", new InstProp(88, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("AND", new InstProp(64, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("COMP", new InstProp(40, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("COMPF", new InstProp(136, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("DIV", new InstProp(36, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("DIVF", new InstProp(100, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("J", new InstProp(60, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("JEQ", new InstProp(48, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("JGT", new InstProp(52, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("JLT", new InstProp(56, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("JSUB", new InstProp(72, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("LDA", new InstProp(0, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("LDB", new InstProp(104, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("LDCH", new InstProp(80, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("LDF", new InstProp(112, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("LDL", new InstProp(8, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("LDS", new InstProp(108, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("LDT", new InstProp(116, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("LDX", new InstProp(4, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("LPS", new InstProp(208, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("MUL", new InstProp(32, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("MULF", new InstProp(96, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("OR", new InstProp(68, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("RD", new InstProp(216, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("RSUB", new InstProp(76, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("TD", new InstProp(224, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("TIX", new InstProp(44, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("WD", new InstProp(220, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("SSK", new InstProp(236, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("STA", new InstProp(12, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("STB", new InstProp(120, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("STCH", new InstProp(84, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("STF", new InstProp(128, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("STI", new InstProp(212, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("STL", new InstProp(20, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("STS", new InstProp(124, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("STSW", new InstProp(232, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("STT", new InstProp(132, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("STX", new InstProp(16, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("SUB", new InstProp(28, Format.FORMAT3, VALUE, NONE));
+        opCodeTable.put("SUBF", new InstProp(92, Format.FORMAT3, VALUE, NONE));
     }
 
     private static void fillAssemblerDirectives() {
--- src/assembler/datastructures/OperandType.java
+++ src/assembler/datastructures/OperandType.java
@@ -4,5 +4,5 @@ package src.assembler.datastructures;
  * Created by ahmed on 4/21/17.
  */
 public enum OperandType {
-    REGISTER, IMMEDIATE, VALUE, NONE
+    REGISTER, VALUE, NONE
 }
