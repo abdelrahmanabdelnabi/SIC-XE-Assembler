166603387b31bfb41cbfd07f0c979fe5aebea5be
Ahmed Lotfey <a7med.lotfey@gmail.com>
2017-04-21 14:52:43 +0200

    Add Operand Type Enum

--- src/assembler/datastructures/InstProp.java
+++ src/assembler/datastructures/InstProp.java
@@ -1,17 +1,33 @@
 package src.assembler.datastructures;
 
 /**
- *
  * Created by abdelrahman on 4/7/17.
  */
 public class InstProp {
-
+    private OperandType FirstOperand;
+    private OperandType SecondOperand;
     private int opCode;
     private Format format;
 
     InstProp(int opCode, Format format) {
         this.opCode = opCode;
         this.format = format;
+    }
+
+    public OperandType getFirstOperand() {
+        return FirstOperand;
+    }
+
+    public void setFirstOperand(OperandType firstOperand) {
+        FirstOperand = firstOperand;
+    }
+
+    public OperandType getSecondOperand() {
+        return SecondOperand;
+    }
+
+    public void setSecondOperand(OperandType secondOperand) {
+        SecondOperand = secondOperand;
     }
 
     int getOpCode() {
---
+++ src/assembler/datastructures/OperandType.java
@@ -0,0 +1,8 @@
+package src.assembler.datastructures;
+
+/**
+ * Created by ahmed on 4/21/17.
+ */
+public enum OperandType {
+    REGISTER, IMMEDIATE, VALUE, NONE
+}
