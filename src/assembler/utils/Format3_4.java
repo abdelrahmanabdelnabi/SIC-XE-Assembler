683a0d501513a01c993176ec0333ed3107937c32
Abdelrahman <abdoumohamed96@gmail.com>
2017-04-21 03:27:10 +0200

    implement flag setters in Format2 and Format3_4

--- src/assembler/utils/Format3_4.java
+++ src/assembler/utils/Format3_4.java
@@ -6,8 +6,15 @@ import src.assembler.datastructures.Format;
  * Created by ahmed on 4/12/17.
  */
 public class Format3_4 extends Format_2 {
-    // TODO implement M
-    // E !
+    // TODO: implement ME
+    protected int operand;
+    private int secondOperand;
+    private boolean isIndirect;
+    private boolean isImmediate;
+    private boolean isIndexed;
+    private boolean isBaseRelative;
+    private boolean isPCRelative;
+
     @Override
     public String toString() {
         return null;
@@ -15,32 +22,37 @@ public class Format3_4 extends Format_2 {
 
     @Override
     void setIndirect(boolean isIndirect) {
+        this.isIndirect = isIndirect;
 
     }
 
     @Override
     void setImmediate(boolean isImmediate) {
+        this.isImmediate = isImmediate;
 
     }
 
     @Override
-    void setIndexed(boolean isIndirect) {
+    void setIndexed(boolean isIndexed) {
+        this.isIndexed = isIndexed;
 
     }
 
     @Override
     void setBaseRelative(boolean isBaseRelative) {
+        this.isBaseRelative = isBaseRelative;
 
     }
 
     @Override
     void setPCRelative(boolean isPCRelative) {
+        this.isPCRelative = isPCRelative;
 
     }
 
     @Override
     void setOperand(int operand) {
-
+        this.operand = operand;
     }
 
     @Override
