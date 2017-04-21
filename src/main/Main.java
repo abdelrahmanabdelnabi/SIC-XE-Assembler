68dc89f48b65b8022f80b0889224e7d75f6f9f50
Abdelrahman <abdoumohamed96@gmail.com>
2017-04-21 05:53:09 +0200

    update main to run the assembler properly

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,8 @@
 package src.main;
 
+import jdk.nashorn.internal.runtime.ParserException;
 import src.assembler.Assembler;
+import src.assembler.AssemblerException;
 import src.assembler.Logger;
 import src.filewriter.FormAboFayezString;
 import src.filewriter.FormObjectString;
@@ -8,47 +10,77 @@ import src.filewriter.FormSymbolsString;
 import src.filewriter.Writer;
 import src.parser.InputReader;
 import src.parser.Parser;
+import src.parser.ParsingException;
 
 /*
  * Created by abdelrahman on 4/9/17.
  */
 public class Main {
     public static void main(String[] args) {
+
         // take argument from command line
-//        String path = args[0];
+        // String path = args[0];
+
         String path = "/home/ahmed/Data/Workspace/IdeaProjects/SIC-XE-Assembler/src/testIn/format2.asm";
+
+        InputReader reader = new InputReader(InputReader.InputType.File, path);
+        Parser parser = new Parser(reader);
+
         try {
-            InputReader reader = new InputReader(InputReader.InputType.File, path);
-            Parser parser = new Parser(reader);
-            Assembler assembler = new Assembler(parser.getParsedInstuctions());
 
-            assembler.assembleCode();
+            parser.parse();
 
-            path = path.replace("testIn", "testOut");
-            // check for errors
-            String errorFile = path.replace(".asm", "_log.txt");
-            String symTab = path.replace(".asm", "_symTab.txt");
-            String aboFayezTab = path.replace(".asm", "_aboFayezTable.txt");
-            String objectFile = path.replace(".asm", ".obj");
-            //
-            Writer writer = new Writer("");
-            // Symbols
-            writer.setFileName(symTab);
-            writer.toFile(new FormSymbolsString(assembler.getSymbolTable()).toString());
-            // object
-            writer.setFileName(objectFile);
-            writer.toFile(new FormObjectString(assembler.getInstructions()).toString());
-            // abo fayez table
-            writer.setFileName(aboFayezTab);
-            writer.toFile(new FormAboFayezString(assembler.getInstructions()).toString());
-            // Log file
-            writer.setFileName(errorFile);
-            writer.toFile(Logger.getLogString());
-        } catch (Exception e) {
-            Writer writer = new Writer("");
-            String errorFile = path.replace(".asm", "_log.txt");
-            writer.setFileName(errorFile);
-            writer.toFile(Logger.getLogString());
+        } catch (ParsingException pe) {
+            // TODO: output errors to stdout as well as log file
+            System.out.println(pe.getMessage());
         }
+
+        Assembler assembler = new Assembler(parser.getParsedInstuctions());
+
+        try {
+
+
+            assembler.executePassOne();
+            assembler.executePassTwo();
+        }
+
+        catch (AssemblerException ae) {
+            // TODO: output errors to stdout as well as log file
+            Writer writer = new Writer("");
+            String errorFile = path.replace(".asm", "_log.txt");
+            writer.setFileName(errorFile);
+            writer.toFile(Logger.getLogString());
+
+            System.out.println(ae.getMessage());
+
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        }
+
+        path = path.replace("testIn", "testOut");
+
+        // check for errors
+        String errorFile = path.replace(".asm", "_log.txt");
+        String symTab = path.replace(".asm", "_symTab.txt");
+        String aboFayezTab = path.replace(".asm", "_aboFayezTable.txt");
+        String objectFile = path.replace(".asm", ".obj");
+
+        Writer writer = new Writer("");
+
+        // Symbols
+        writer.setFileName(symTab);
+        writer.toFile(new FormSymbolsString(assembler.getSymbolTable()).toString());
+
+        // object
+        writer.setFileName(objectFile);
+        writer.toFile(new FormObjectString(assembler.getInstructions()).toString());
+
+        // abo fayez table
+        writer.setFileName(aboFayezTab);
+        writer.toFile(new FormAboFayezString(assembler.getInstructions()).toString());
+
+        // Log file
+        writer.setFileName(errorFile);
+        writer.toFile(Logger.getLogString());
     }
 }
