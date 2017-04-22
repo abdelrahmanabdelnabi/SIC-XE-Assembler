f79edd954d3a98ceab0ae694fd10c8617a73866b
Ahmed Lotfey <a7med.lotfey@gmail.com>
2017-04-22 18:31:21 -0400

    ListingString Fix

--- src/filewriter/ListingString.java
+++ src/filewriter/ListingString.java
@@ -25,25 +25,29 @@ public class ListingString implements StringGenerator {
 
     private void formString() {
         // 90
-        data.append("================================================================================================\n");
+        data.append(getSeperator());
         // 4 line - 10 loc cnt - 30 label - 10 mnemonic - 20 operand-10 object
-        data.append(String.format("%-3s| %-10s| %-30s| %-10s| %-20s| %-10s\n", "Line", "LOC_CTR",
+        data.append(String.format("%-3s| %-10s| %-20s| %-10s| %-20s| %-10s\n", "Line", "LOC_CTR",
                 "LABEL", "MNEMONIC", "OPERAND", "Object Code"));
-        data.append("================================================================================================\n");
+        data.append(getSeperator());
 
         for (Instruction instruction : instructions) {
             data.append(formInstructionString(instruction));
         }
-        data.append("================================================================================================\n");
+        data.append(getSeperator());
 
     }
 
     private String formInstructionString(Instruction inst) {
-        return String.format(" %-3s| %-10s| %-30s| %-10s| %-20s| %-10s\n", Integer.toString(inst.getLineNumber()),
+        return String.format(" %-3s| %-10s| %-20s| %-10s| %-20s| %-10s\n", Integer.toString(inst.getLineNumber()),
                 extendToLength(Integer.toHexString(inst.getAddress()), 6),
                 inst.getLabel(),
                 inst.getMnemonic(),
                 inst.getOperand(),
                 inst.getObjectCode());
+    }
+
+    private String getSeperator() {
+        return "======================================================================================\n";
     }
 }
