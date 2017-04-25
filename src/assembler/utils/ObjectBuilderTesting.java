cb3ed0957ba05bf23aa94f13bd008b37ddd56d0e
Abdelrahman <abdoumohamed96@gmail.com>
2017-04-25 02:02:30 +0200

    add tests for format 1 builder

--- src/assembler/utils/ObjectBuilderTesting.java
+++ src/assembler/utils/ObjectBuilderTesting.java
@@ -3,8 +3,11 @@ package src.assembler.utils;
 import org.junit.Test;
 import src.assembler.Instruction;
 import src.assembler.core.Assembler;
+import src.assembler.datastructures.OpcodeTable;
 import src.parser.InputReader;
 import src.parser.Parser;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by ahmed on 4/12/17.
@@ -17,12 +20,6 @@ public class ObjectBuilderTesting {
     private String format4_FilePath = relativePath + "/src/testIn/format4.asm";
     private Parser parser;
     private InputReader reader;
-
-//    @Before
-//    public void setUp() {
-//        reader = new InputReader(InputReader.InputType.File, format1_FilePath);
-//        parser = new Parser(reader);
-//    }
 
     @Test
     public void testFormat_1() {
@@ -59,5 +56,25 @@ public class ObjectBuilderTesting {
         for (Instruction inst : assembler.getInstructions()) {
             System.out.println(inst.getObjectCode());
         }
+    }
+
+    @Test
+    public void testFormatFIX() {
+        // FIX
+        String obj = ObjectBuilder.buildFormatOne(OpcodeTable.getOpCode("FIX"));
+
+        assertEquals("C4", obj);
+    }
+
+    @Test
+    public void testFLOAT() {
+        String obj = ObjectBuilder.buildFormatOne(OpcodeTable.getOpCode("FLOAT"));
+        assertEquals("C0", obj);
+    }
+
+    @Test
+    public void testHIO() {
+        String obj = ObjectBuilder.buildFormatOne(OpcodeTable.getOpCode("HIO"));
+        assertEquals("F4", obj);
     }
 }
