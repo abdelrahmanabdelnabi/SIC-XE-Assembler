bcfa3fabb6049e957a2f604795f0736ad2a94e26
Ahmed Lotfey <a7med.lotfey@gmail.com>
2017-04-28 15:59:41 +0200

    object string fix, lexicalAnalysis class

---
+++ src/parser/LexicalAnalyser.java
@@ -0,0 +1,33 @@
+package src.parser;
+
+/**
+ * Created by ahmed on 4/28/17.
+ */
+
+import src.assembler.Instruction;
+
+import java.util.ArrayList;
+
+/**
+ * This Class is responsible for code syntax analysis
+ * Detects lexical errors in the input code
+ * Takes an ArrayList of Instructions, analyze it, then sets the Error Flag at
+ * Logger Class, Using Logger.LogError(msg);
+ */
+public class LexicalAnalyser {
+    private ArrayList<Instruction> srcCode;
+
+    LexicalAnalyser(ArrayList<Instruction> srcCode) {
+        this.srcCode = srcCode;
+    }
+
+    public void inspectCode() {
+        for (Instruction inst : srcCode) {
+            validateSyntax(inst);
+        }
+    }
+
+    private void validateSyntax(Instruction inst) {
+
+    }
+}
