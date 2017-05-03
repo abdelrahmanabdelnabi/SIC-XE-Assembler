79e4900decdae8a7d4cf1e7db641027fe7bba6ef
Abdelrahman <abdoumohamed96@gmail.com>
2017-05-03 18:32:49 +0200

    fix invalid files being overwritten

--- sicXE/Asm.java
+++ sicXE/Asm.java
@@ -26,74 +26,72 @@ class Asm {
         String relativePath = System.getProperty("user.dir") + "/" + args[0];
         String absolutePath = args[0];
         String filePath = lockFile(relativePath, absolutePath);
-        // change file filePath to change test file
+
+        // change filePath to change test file
 //        String relativePath = System.getProperty("user.dir");// + "/" + args[0];
 //        String filePath = relativePath + "/src/tests/PROG_FORMAT4/PROG_FORMAT4.asm";
 
-        InputReader reader = null;
-        // create file reader
-        if (Logger.getErrorsCnt() == 0)
-            reader = new InputReader(InputReader.InputType.File, filePath);
-        else {
-            System.out.println(Logger.getLogString());
-            System.exit(-1);
-        }
+        if (filePath.length() != 0) {
 
-        // create parser
-        Parser parser = new Parser(reader);
-        // parse file
-        try {
-            parser.parse();
-        } catch (ParsingException pe) {
-            // TODO: output errors to stdout as well as log file
-            System.out.println("Parsing Error: " + pe.getMessage());
-        }
+            // create file reader
+            InputReader reader = new InputReader(InputReader.InputType.File, filePath);
 
-
-        // Validate Syntax Check
-        LexicalAnalyzer analyzer = new LexicalAnalyzer(parser.getParsedInstuctions());
-        analyzer.inspectCode();
-
-        // check if no syntax errors found
-        if (Logger.getErrorsCnt() == 0) {
-            // continue assembling file
-            assembler = new Assembler(parser.getParsedInstuctions());
+            // create parser
+            Parser parser = new Parser(reader);
+            // parse file
             try {
-                assembler.executePassOne();
-                assembler.executePassTwo();
-            } catch (AssemblerException ae) {
+                parser.parse();
+            } catch (ParsingException pe) {
                 // TODO: output errors to stdout as well as log file
-                System.out.println("Assembling Error: " + ae.getMessage());
-            } catch (Exception e) {
-                e.printStackTrace(System.err);
+                System.out.println("Parsing Error: " + pe.getMessage());
             }
-        }
 
-        // end assembling and out Log file
-        String errorFile = filePath.replace(".asm", "_log.txt");
-        String symTabFile = filePath.replace(".asm", "_symTab.txt");
-        String lstTabFile = filePath.replace(".asm", "_LstFile.txt");
-        String objectFile = filePath.replace(".asm", "_obj.txt");
+
+            // Validate Syntax Check
+            LexicalAnalyzer analyzer = new LexicalAnalyzer(parser.getParsedInstuctions());
+            analyzer.inspectCode();
+
+            // check if no syntax errors found
+            if (Logger.getErrorsCnt() == 0) {
+                // continue assembling file
+                assembler = new Assembler(parser.getParsedInstuctions());
+                try {
+                    assembler.executePassOne();
+                    assembler.executePassTwo();
+                } catch (AssemblerException ae) {
+                    // TODO: output errors to stdout as well as log file
+                    System.out.println("Assembling Error: " + ae.getMessage());
+                } catch (Exception e) {
+                    e.printStackTrace(System.err);
+                }
+            }
+
+            // end assembling and out Log file
+            String errorFile = filePath.replace(".asm", "_log.txt");
+            String symTabFile = filePath.replace(".asm", "_symTab.txt");
+            String lstTabFile = filePath.replace(".asm", "_LstFile.txt");
+            String objectFile = filePath.replace(".asm", "_obj.txt");
 
         /* if there's no errors found, write data to files
                 else write only log file */
 
-        if (Logger.getErrorsCnt() == 0) {
-            // Symbols
-            writer.setFileName(symTabFile);
-            writer.writeToFile(new SymbolsString(assembler.getSymbolTable(), assembler.getLiteralsTable()).toString());
+            if (Logger.getErrorsCnt() == 0) {
+                // Symbols
+                writer.setFileName(symTabFile);
+                writer.writeToFile(new SymbolsString(assembler.getSymbolTable(), assembler.getLiteralsTable()).toString());
 
-            // object
-            writer.setFileName(objectFile);
-            writer.writeToFile(assembler.getObjectCode());
+                // object
+                writer.setFileName(objectFile);
+                writer.writeToFile(assembler.getObjectCode());
 
-            // abo listing table
-            writer.setFileName(lstTabFile);
-            writer.writeToFile(new ListingString(assembler.getInstructions()).toString());
+                // abo listing table
+                writer.setFileName(lstTabFile);
+                writer.writeToFile(new ListingString(assembler.getInstructions()).toString());
+            }
+            // Log file
+            writer.setFileName(errorFile);
+            writer.writeToFile(Logger.getLogString());
         }
-        // Log file
-        writer.setFileName(errorFile);
-        writer.writeToFile(Logger.getLogString());
     }
 
     private static String lockFile(String relativePath, String absolutePath) {
